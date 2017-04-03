ca7cb9dda8606a3ea70ab99335294f891806ed00
dmitrui98 <dmitrui98@mail.ru>
2017-04-04 00:20:58 +0300

    connecting to DB

---
+++ persistence/src/main/java/by/dmitrui98/entity/Guest.java
@@ -0,0 +1,7 @@
+package by.dmitrui98.entity;
+
+/**
+ * Created by Администратор on 03.04.2017.
+ */
+public class Guest {
+}
---
+++ persistence/src/main/java/by/dmitrui98/entity/User.java
@@ -0,0 +1,152 @@
+package by.dmitrui98.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by Администратор on 03.04.2017.
+ */
+
+@Entity
+@Table(name = "user1")
+public class User {
+
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    private long user_id;
+
+    @Column(name = "login", nullable = false, length = 100)
+    private String login;
+
+    @Column(name = "email", nullable = false, length = 100)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 100)
+    private String password;
+
+    @Column(name = "fname")
+    private String fname;
+
+    @Column(name = "lname")
+    private String lname;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "discont")
+    private double discont;
+
+    @Column(name = "create_at")
+    private Date createAt;
+
+    @Column(name = "update_at")
+    private Date updateAt;
+
+
+    public User() {
+    }
+
+    public User(String login, String email, String password, String fname, String lname, String surname, String phone) {
+        this.login = login;
+        this.email = email;
+        this.password = password;
+        this.fname = fname;
+        this.lname = lname;
+        this.surname = surname;
+        this.phone = phone;
+    }
+
+    public long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(long user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public double getDiscont() {
+        return discont;
+    }
+
+    public void setDiscont(double discont) {
+        this.discont = discont;
+    }
+
+    public Date getCreate_at() {
+        return createAt;
+    }
+
+    public void setCreate_at(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Date getUpdate_at() {
+        return updateAt;
+    }
+
+    public void setUpdate_at(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+}
